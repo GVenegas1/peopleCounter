9eb109db323accaabd89464219474157ce8b5fb7
Gabriel-Venegas <Gabrielvenegas.co@gmail.com>
2022-08-31 17:49:24 -0700

    count

---
+++ practice-one.js
@@ -0,0 +1,26 @@
+
+
+let countEl = document.getElementById("count-el") //pass argument 
+let saveEl = document.getElementById("save-el")
+console.log(saveEl)
+
+let count = 0
+function increment() {
+
+    count += 1
+    countEl.textContent = count
+}
+
+
+function save() {
+
+let countStr = count + " - "
+saveEl.textContent += countStr 
+countEl.textContent = 0 
+count = 0
+
+}
+
+
+
+
